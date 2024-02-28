20364dd70ca2aace765acf687b52925ad718b0c6
Danil Konovalenko <dkonovalenko@wooppay.kz>
2024-02-28 16:44:47 +0600

    add remove position camera

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -2,7 +2,8 @@
   import { T } from '@threlte/core'
   import { OrbitControls, Stars } from '@threlte/extras'
   import Tulips from '$lib/components/models/Tulips.svelte';
-
+  import { onMount } from 'svelte';
+  import { PerspectiveCamera } from 'three';
 
   export let autoRotate: boolean
   export let enableDamping: boolean
@@ -12,6 +13,23 @@
   export let minPolarAngle: number
   export let maxPolarAngle: number
   export let enableZoom: boolean
+
+  let counter = 0;
+  let cameraRef :  PerspectiveCamera ;
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      if (counter < 250) {
+        counter += 1;
+      } else {
+        clearInterval(interval);
+      }}, 7); // 1000 ms = 1 second
+  });
+
+  // Reactive statement to update camera position when counter changes
+  $: if (cameraRef) {
+    cameraRef.position.set(counter, counter, counter);
+  }
 </script>
 
 <Stars />
@@ -20,7 +38,9 @@
   makeDefault
   position={[0, 1, 1]}
   fov={90}
-
+  on:create={({ ref }) => {
+   cameraRef = ref;
+  }}
 >
   <OrbitControls
     {enableDamping}
