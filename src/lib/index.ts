dbb37b69fca4c11aefeadceae1f3cf3d1c17306a
Danil Konovalenko <dkonovalenko@wooppay.kz>
2024-02-28 11:55:57 +0600

    add model

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,2 @@
 // place files you want to import through the `$lib` alias in this folder.
+import Tulips from '$lib/components/models/Tulips.svelte';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,5 @@
     width: 100vw;
     height: 100vh;
     background: rgb(13, 19, 32);
-    background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
   }
 </style>
