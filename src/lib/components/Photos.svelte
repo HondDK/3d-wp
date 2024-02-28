4a8686dffb969de5204a08904310958f2fc596ed
Danil Konovalenko <dkonovalenko@wooppay.kz>
2024-02-28 17:24:07 +0600

    added a photo by radius

---
+++ src/lib/components/Photos.svelte
@@ -0,0 +1,16 @@
+<script>
+  import {Billboard, HTML} from "@threlte/extras";
+
+  export let position = [0, 0, 0]
+  export let imageSrc = ''
+</script>
+
+<Billboard follow={false} >
+  <HTML
+    scale={15}
+    transform
+    {position}
+  >
+    <img alt='aboba' src={imageSrc} />
+  </HTML>
+</Billboard>
